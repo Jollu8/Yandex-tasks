6e9a031a27b541fcb17ad76793b3cac71b4ed43a
Jollu8 <zholu8@gmail.com>
2023-07-23 00:09:07 +0300

    !

---
+++ practikum2022/sprint1_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string_view>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int minimumOperations(vector<int>& nums) {
+    int size = 0;
+    int res = 0;
+    while(size <= nums.size()) {
+        for(auto &i:nums) {
+            if()
+        }
+    }
+
+}
+
+int main() {
+    cout << numberOfSteps(123);
+}
---
+++ practikum2022/sprint_1_2.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long int a,x,b,c;
+    cin >> a >> x >> b >> c;
+    std::cout << ((a*(pow(x,2))) + (b*x) + c ) << endl;
+}
